96b038f9a30843c5882d49cafe08d984d279c9c0
Ulf Tietze <u.tietze@neusta.de>
2019-06-01 12:45:21 +0200

    Refactore Console Command Directory

---
+++ Console/ExportCommand.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace Firegento\ContentProvisioning\Console;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ExportCommand extends Command
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+
+    }
+}
