a05245c9f49b4f2239a10b2f542ff6f5e434aa5b
Harald Deiser <h.deiser@techdivision.com>
2019-06-01 16:58:01 +0200

    Added backend reset button resolve #9

---
+++ Model/Query/HasDefaultBlockConfiguration.php
@@ -0,0 +1,48 @@
+<?php
+declare(strict_types=1);
+
+namespace Firegento\ContentProvisioning\Model\Query;
+
+use Magento\Cms\Api\BlockRepositoryInterface;
+use Magento\Framework\Exception\LocalizedException;
+
+class HasDefaultBlockConfiguration
+{
+    /**
+     * @var BlockRepositoryInterface
+     */
+    private $blockRepository;
+
+    /**
+     * @var GetBlockEntryByBlock
+     */
+    private $getBlockEntryByBlock;
+
+    /**
+     * @param BlockRepositoryInterface $pageRepository
+     * @param GetBlockEntryByBlock $getPageEntryByPage
+     */
+    public function __construct(
+        BlockRepositoryInterface $pageRepository,
+        GetBlockEntryByBlock $getPageEntryByPage
+    ) {
+        $this->blockRepository = $pageRepository;
+
+        $this->getBlockEntryByBlock = $getPageEntryByPage;
+    }
+
+    /**
+     * @param int $entityId
+     * @return bool
+     */
+    public function get(int $entityId): bool
+    {
+        try {
+            $block = $this->blockRepository->getById($entityId);
+            $entry = $this->getBlockEntryByBlock->execute($block);
+            return $entry ? true : false;
+        } catch (LocalizedException $noSuchEntityException) {
+            return false;
+        }
+    }
+}
